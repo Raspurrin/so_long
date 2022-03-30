c59afa0c39b2e09c5ec0aa8394542de7ca6c242d
Raspurrin <vondetou@gmail.com>
2022-03-30 23:25:27 +0200

    Removed an include because getnextline is included in libft.

--- so_long.h
+++ so_long.h
@@ -6,14 +6,13 @@
 /*   By: mialbert <mialbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 21:19:30 by mialbert          #+#    #+#             */
-/*   Updated: 2022/03/29 21:19:32 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:58:44 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "get_next_line/get_next_line.h"
 # include "libft/srcs/libft.h"
 
 struct	s_checks
