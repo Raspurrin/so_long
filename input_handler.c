13dfea4954b04d3ee9ab9e71d170df5bba1fb59a
Raspurrin <vondetou@gmail.com>
2022-03-31 02:58:04 +0200

    Changing approach, in process. Reading everything at first and then using ft_split.

--- input_handler.c
+++ input_handler.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 14:59:12 by mialbert          #+#    #+#             */
-/*   Updated: 2022/03/30 20:15:43 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/03/31 02:57:11 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,29 +60,46 @@ static bool	error_check(char *line, int32_t linecount, int32_t size,
 	return (true);
 }
 
+/**
+ * char **name; 
+ * 
+ * name[1] = "hello";
+ * name[2] = "bro";
+ */
+
+size_t	check_if_rectangular(char **map)
+{
+	size_t	i;
+	size_t	size;
+
+	i = 0;
+	size = 0;
+
+	while (map[i])
+	{
+		size = ft_strlen(map[i++]);
+		if (size != ft_strlen(map[i++]))
+			return (ft_putendl_fd("Error\n", STDOUT_FILENO), false);
+		i++;
+	}
+	return (i);
+}
+
 char	**input_handler(int32_t fd, struct s_checks checks, char **map)
 {
 	size_t				i;
 	size_t				size;
 	char				*line;
+	char				*buf;
 
 	size = 0;
 	i = 0;
 	while (1)
 	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		map = malloc(i * sizeof(char *));
-		if (i == 0 || (i > 1 && ft_strlen(line) == size))
-		{
-			size = ft_strlen(line);
-			ft_strlcpy(map[i], line, size);
-		}
-		else
-			return (NULL);
+		map = ft_split(read(fd, buf, 5000), '\n');
+		check_if_rectangular(map);
 		if (!(error_check(line, i, size, checks)))
-			display_error(map);
+			display_error(map); // now I have to rethink my error_check function
 		i++;
 	}
 	error_check(line, -1, size, checks);
@@ -139,4 +156,138 @@ int32_t	main(int32_t argc, char **argv)
  * Just... Read everything? And then split. Array size problem possibly
  * First count amount of lines before mallocing
  * Linked lists
+ * 
+ * Option 1:
+ * First read line by line using get_next_line to get the number of lines
+ * And *then* malloc appropiate number of pointers and initialize everything
+ * 
+ * OR 
+ * 
+ * Option 2:
+ * get everything into one big string, use ft_split and then go through the outer
+ * elements to count the size
  */
+
+
+
+
+
+
+// just in case:
+
+
+
+
+// #include "so_long.h"
+// #include "./libft/srcs/libft.h"
+
+// char	*display_error(char **map)
+// {
+// 	int32_t	i;
+
+// 	i = 0;
+// 	ft_putendl_fd("Error\n", STDOUT_FILENO);
+// 	while (*map)
+// 	{
+// 		free(*map);
+// 		map++;
+// 	}
+// 	return (NULL);
+// }
+
+/**
+ * @brief Checks if one line read contains the expected chararcters or not
+ * @param line The line to be checked
+ * @param check The string containing all expected characters
+ * @return True if no characters from the check string have been found, 
+ * otherwise false.
+ */
+
+// static bool	error_check(char *line, int32_t linecount, int32_t size,
+// 						struct s_checks checks)
+// {
+// 	int32_t	i;
+
+// 	i = 0;
+// 	if (linecount <= 0 && !(ft_strchr(line, '1')))
+// 		return (ft_putendl_fd("Error\n", STDOUT_FILENO), false);
+// 	else
+// 		return (true);
+// 	if (i == 0 || i == size - 1)
+// 	{
+// 		if (line[i] != '1')
+// 			return (ft_putendl_fd("Error\n", STDOUT_FILENO), false);
+// 	}
+// 	else if (!(ft_strchr("01CEP", line[i++])))
+// 		return (ft_putendl_fd("Error\n", STDOUT_FILENO), false);
+// 	else if (ft_strchr(line, 'C'))
+// 		checks.collectible++;
+// 	else if (ft_strchr(line, 'P'))
+// 		checks.start++;
+// 	else if (ft_strchr(line, 'E'))
+// 		checks.exit++;
+// 	return (true);
+// }
+
+// size_t	get_line_number(int32_t fd, char *line)
+// {
+// 	char	*line;
+
+	
+// 	}
+	
+// }
+
+// char	**input_handler(int32_t fd, struct s_checks checks, char **map)
+// {
+// 	size_t				i;
+// 	size_t				size;
+// 	char				*line;
+
+// 	size = 0;
+// 	i = 0;
+// 	while (1)
+// 	{
+// 		line = get_next_line(fd);
+// 		if (!line)
+// 			break ;
+// 		map = malloc(i * sizeof(char *));
+// 		if (i == 0 || (i > 1 && ft_strlen(line) == size))
+// 		{
+// 			size = ft_strlen(line);
+// 			ft_strlcpy(map[i], line, size);
+// 		}
+// 		else
+// 			return (NULL);
+// 		if (!(error_check(line, i, size, checks)))
+// 			display_error(map);
+// 		i++;
+// 	}
+// 	error_check(line, -1, size, checks);
+// 	return (map);
+// }
+
+// int32_t	main(int32_t argc, char **argv)
+// {
+// 	char			**map;
+// 	int32_t			fd;
+// 	struct s_checks	checks;
+
+// 	map = NULL;
+// 	checks.collectible = 0;
+// 	checks.exit = 0;
+// 	checks.start = 0;
+// 	if (argc == 2)
+// 	{
+// 		fd = open(argv[1], O_RDONLY);
+// 		if (fd == -1)
+// 		{
+// 			ft_putendl_fd("Error\n Invalid file\n", STDOUT_FILENO);
+// 			return (EXIT_FAILURE);
+// 		}
+// 		input_handler(fd, checks, map);
+// 		if (!(checks.collectible >= 1 && checks.exit >= 1 && checks.start >= 1))
+// 			display_error(map);
+// 	}
+// 	return (0);
+// }
