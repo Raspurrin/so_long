9e0991aae22f72e7d8d3001021b43bc5c77243f5
Raspurrin <vondetou@gmail.com>
2022-04-05 22:47:48 +0200

    Changed paths.

--- Makefile
+++ Makefile
@@ -6,19 +6,19 @@
 #    By: mialbert <mialbert@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/30 00:41:49 by mialbert          #+#    #+#              #
-#    Updated: 2022/04/04 21:43:12 by mialbert         ###   ########.fr        #
+#    Updated: 2022/04/05 22:39:54 by mialbert         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CFLAGS = -Wall -Werror -Wextra
 NAME = so_long
-SRCS = input_handler.c free.c
+SRCS = ./srcs/input_handler.c ./srcs/free.c ./libs/libft/srcs/*.c ./srcs/main.c ./srcs/error_cases.c
 
 all : $(NAME)
 
 $(NAME) :
-	$(MAKE) -C libft/srcs
-	$(CC) $(CFLAGS) -g $(SRCS) libft/srcs/libft.a -o $(NAME)
+	$(MAKE) -C ./libs/libft/srcs
+	$(CC) $(CFLAGS) -g -I includes -I libs/libft/srcs $(SRCS) ./libs/libft/srcs/libft.a -o $(NAME)
 #	#./so_long map.ber
 
 clean: 
--- includes/so_long.h
+++ includes/so_long.h
@@ -6,20 +6,36 @@
 /*   By: mialbert <mialbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 21:19:30 by mialbert          #+#    #+#             */
-/*   Updated: 2022/04/04 21:13:33 by mialbert         ###   ########.fr       */
+/*   Updated: 2022/04/05 22:40:29 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "libft/srcs/libft.h"
+# include "libft.h"
+# include <stdio.h>
+
+typedef struct error_cases
+{
+	bool	error;
+	bool	file_name;
+	bool	walls;
+	bool	cpe;
+	bool	different_input;
+	bool	rectangular;
+}	t_error;
 
 /**
  * @brief Iterates through an array of strings and frees every string
  * @param map 2D array
  * @return NULL
  */
-char	*free_2d(char **map);
+
+char	*read_file(int32_t fd, char *line);
+void	free_2d(char **map);
+bool	check_ext(char *file_name, char *ext);
+char	**input_handler(int32_t fd, char **map);
+void	error_output(t_error *errors, size_t linecount);
 
 #endif
